508d1f9268a611d3d7b11aaa2ad8995e0e9eb69c
VishalKK1997 <vishalkumarkharwar@gmail.com>
2020-12-14 19:36:28 +0530

    sessions

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -40,10 +41,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length < 2) {
       return {
@@ -87,13 +97,18 @@ export class UserResolver {
       }
     }
 
+    // Store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -107,6 +122,8 @@ export class UserResolver {
         errors: [{ field: "password", message: "Incorrect password" }],
       };
     }
+
+    req.session.userId = user.id;
 
     return {
       user,
